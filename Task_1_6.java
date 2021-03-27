413d28e8a5595d70a380607cde014bfaa5e02f49
ToxaRyd <65728417+ToxaRyd@users.noreply.github.com>
2021-03-27 14:19:08 +0300

    Add files via upload

---
+++ Task_1_6.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main (String[] args)
+    {
+        Scanner num = new Scanner(System.in);
+        int x, y;
+        boolean r = false;
+        System.out.print("x = ");
+        x = num.nextInt();
+        System.out.print("y = ");
+        y = num.nextInt();
+
+
+        if (0 <= y && y <= 4) {
+            if (x >= -2 && x <= 2) r = true;
+        }
+        else if (y >= -3 && y <= 0) {
+            if (x >= -4 && x <= 4) r = true;
+        }
+        
+        System.out.println(r);
+    }
+}
+
