cdb8b1190a8d15075bf75bf906ba34ab6cf79b2b
ToxaRyd <65728417+ToxaRyd@users.noreply.github.com>
2021-04-23 20:59:59 +0300

    Add files via upload

---
+++ Task_2_3_2.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main (String[] args) {
+        Scanner num = new Scanner(System.in);
+        int [] a = new int[] {1, 2, 3, 7, 8, 9};
+        int [] b = new int[] {4, 5, 6};
+        int k = 1, x;
+
+        // Объединение :D
+        int c, d, e;
+        c = b[0];
+        d = b[1];
+        e = b[2];
+        b = new int[9];
+
+        for (int i=0; i < b.length; i++) {
+            if (i <= k) {
+                b[i] = a[i];
+            } else {
+                b[i] = c;
+                b[i+1] = d;
+                b[i+2] = e;
+                for (int j=i+3; j < b.length; j++) {
+                    b[j] = a[j-3];
+                }
+                break;
+            }
+        }
+        //Сортировка
+        for (int i = 0; i < b.length; i++) {
+            for (int j = i; j < b.length; j++) {
+                if (b[i] > b[j]) {
+                    x = b[i];
+                    b[i] = b[j];
+                    b[j]= x;
+                }
+            }
+        }
+        // Вывод массива
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " ");
+        }
+    }
+}
