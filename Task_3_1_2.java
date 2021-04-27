705038425f2369bd6e89f13e2536515a36029920
ToxaRyd <65728417+ToxaRyd@users.noreply.github.com>
2021-04-27 21:00:44 +0300

    Add files via upload

---
+++ Task_3_1_2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.lang.*;
+
+class Main
+{
+    public static void main (String[] args) {
+        String s = "wordword wordwordword word wordword".replace("word", "letter");
+        System.out.println(s);
+    }
+}
