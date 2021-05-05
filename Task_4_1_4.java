718f97f0911585a91e592f11cb25335278fb2591
ToxaRyd <65728417+ToxaRyd@users.noreply.github.com>
2021-05-05 23:22:00 +0300

    Add files via upload

---
+++ Task_4_1_4.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Train {
+    int trNumber;
+    String dest;
+    int depar;
+
+    public Train(int a, String b, int c) {
+        this.trNumber = a;
+        this.dest = b;
+        this.depar = c;
+    }
+}
