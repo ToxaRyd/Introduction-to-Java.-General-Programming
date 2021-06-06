79ca5048c901da5affc94f01b9bc75259a8efa71
ToxaRyd <65728417+ToxaRyd@users.noreply.github.com>
2021-06-06 16:43:17 +0300

    Add files via upload

---
+++ Task_4_1_5_Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+class Main
+{
+    public static void main (String[] args) {
+        Counter a1 = new Counter(10);
+        a1.show();
+        a1.increase();
+        a1.decrease();
+        a1.show();
+    }
+}
